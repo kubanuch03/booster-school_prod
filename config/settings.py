f7a3bb82f20ab3eaf91d856dbcf97b2092f250ee
kubanuch03 <zazaka71@gmail.com>
2024-05-05 21:43:57 +0600

    fix/events image url

--- config/settings.py
+++ config/settings.py
@@ -192,5 +192,5 @@ CORS_ALLOW_HEADERS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_USE_SESSIONS = True
-CSRF_TRUSTED_ORIGINS = ["https://back.horse-travel.com"]
+# CSRF_USE_SESSIONS = True
+# CSRF_TRUSTED_ORIGINS = ["https://back.horse-travel.com"]
